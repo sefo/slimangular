9aec7debb5435396002a985f0dfb9a8f704a22ab
sefo <fournierse@gmail.com>
2017-03-13 14:16:07 +0100

    demo material design dialog ignore *.log

--- angular/components/inner/inner.component.js
+++ angular/components/inner/inner.component.js
@@ -4,16 +4,29 @@ var inner = {
     templateUrl: 'angular/components/inner/inner.template.html'
 };
 
-function innerController(innerService) {
+function innerController(innerService, $mdDialog) {
 
     var vm = this;
 
     this.value = 'dynamic value';
     this.utilisateurs = [];
 
+    this.showAlert = showAlert;
+
     innerService.getUtilisateurs().then(function(response) {
         vm.utilisateurs = response.data;
     });
+
+    function showAlert() {
+        $mdDialog.show(
+            $mdDialog.alert()
+            .clickOutsideToClose(true)
+            .title('This is an alert title')
+            .textContent('You can specify some description text in here.')
+            .ariaLabel('Alert Dialog Demo')
+            .ok('Got it!')
+        );
+    }
 }
 
 angular
